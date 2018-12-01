4334b2fb5e18091b7f588801ef71964b4cc2dfe1
grorge <g97116@yahoo.com>
2018-12-01 21:39:31 +0800

    增加專案

---
+++ app/src/main/java/com/example/tsaohanwen/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tsaohanwen.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText Et1;
+    private TextView title1;
+    private Button button1;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.button1);
+        title1 = (TextView)findViewById(R.id.title1);
+        Et1 = (EditText)findViewById(R.id.Et1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                title1.setText(Et1.getText().toString());
+            }
+        });
+    }
+}
