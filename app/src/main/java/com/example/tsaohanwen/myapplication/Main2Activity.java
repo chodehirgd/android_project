14189c286bc18904896d9ca1f6a7dbfb2efb6bcc
grorge <g97116@yahoo.com>
2018-12-02 11:25:14 +0800

    test transfer value

--- app/src/main/java/com/example/tsaohanwen/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/tsaohanwen/myapplication/Main2Activity.java
@@ -7,24 +7,35 @@ import android.view.View;
 import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
-
-    private Button button1;
+    private String value="test input";
+    private Button button1,button2;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         //取得此Button的實體
        button1 = (Button)findViewById(R.id.button1);
-
+       button2 = (Button)findViewById(R.id.button2);
         //實做OnClickListener界面
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //初始化Intent物件
-                Intent intent = new Intent();
-                //從MainActivity 到Main2Activity
-                intent.setClass( Main2Activity.this,MainActivity.class );
-                //開啟Activity
-                startActivity(intent);
+                value="test input";
+                StartNewActivity();
             }
         });
+        button2.setOnClickListener(new View.OnClickListener(){
+           public void onClick(View v){
+               value="test transfer value";
+               StartNewActivity();
+           }
+        });
+    }
+    protected void StartNewActivity(){
+        //初始化Intent物件
+        Intent intent = new Intent();
+        //從MainActivity 到Main2Activity
+        intent.putExtra("value" , value);
+        intent.setClass( Main2Activity.this,MainActivity.class );
+        //開啟Activity
+        startActivity(intent);
     }
 }
